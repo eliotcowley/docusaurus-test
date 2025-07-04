857a47cbe1c4fd3b4d377371944b20a79607d095
Eliot C <eliot@eliotcowley.dev>
2025-07-04 11:59:04 +0900

    Added video and intro

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import type {ReactNode} from 'react';
+import type { ReactNode } from 'react';
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
@@ -9,7 +9,7 @@ import Heading from '@theme/Heading';
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx('hero', styles.heroBanner)}>
       <div className="container">
@@ -25,20 +25,41 @@ function HomepageHeader() {
             📖 Read my blog
           </Link>
         </div>
+        <div className="ratio ratio-16x9 margin-top--lg">
+          <iframe
+            src="https://www.youtube.com/embed/HXnrO7fs9f0?rel=0"
+            title="Portfolio YouTube video"
+            allowFullScreen={true}
+            width="960"
+            height="540"
+          ></iframe>
+        </div>
       </div>
     </header>
   );
 }
 
 export default function Home(): ReactNode {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
-        <HomepageFeatures />
+        <div className="container">
+          <div className="row">
+            <div className="col col--8 col--offset-2">
+              <p>
+                Hi, I'm Eliot! I've worked as a Technical Writer at companies like Microsoft, Sony, and Indeed, and I have a passion for making games.
+              </p>
+              <p>
+                This portfolio is where I show the documentation projects I've worked on at various companies, games I've made, skills I've learned, and experience I've gained.
+              </p>
+            </div>
+          </div>
+
+        </div>
       </main>
     </Layout>
   );
